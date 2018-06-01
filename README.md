04400bf83af66339a6d27070d752928c95416f57
James Darling <jdarling@credera.com>
2018-06-01 00:16:16 -0500

    Updated output, README, and command line arguments.

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
 # PyBall
-A machine learning program written in Python that can tell the difference between basketball players based on stats
+A machine learning program written in Python that can tell the difference between two different basketball players based on stats
 
-To run the program, you will need to install numPy, sciPy, and scikit-learn
+To run the program, you will need to install numPy, sciPy, and scikit-learn (enter pip [package name] into the command line).
+
+#Training Data and Test Data
+The program will use a set of data for each player known as training data. Tranining data prepares the model to make a prediciton for the test data. 
+
+#Command Line
+The command line follows this format:
+python main.py [Last Row # for Player 1 Train] [Last Row # for Player 1 Test] [Last Row # for Player 2 Train] [Last Row # for Player 2 Test]
+
+So for the example provided (which uses the first 30 games as training data), you'd juse the following command:
+python main.py 30 74 104 152
+
+#CSV Format
+The first column should contain a 0 or 1 to donote if the data belongs to the first or second data. After that, you can use as many different columns of statistics as you want (so long as they are contiguous).
--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from sklearn import tree
 import csv
+import argparse
 
+#Command line arguments that determine which rows of data corrspond to which player's test and train categories
+# EXAMPLE: [Last Row # for Player 1 Train] [Last Row # for Player 1 Test] [Last Row # for Player 2 Train] [Last Row # for Player 2 Test]
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer for the accumulator')
+args = parser.parse_args()
+rows = args.integers
+
+#Reads in data from csv
 allData = []
 features = []
 with open('MachineBasketball.csv', 'r') as f:
@@ -9,6 +18,7 @@ with open('MachineBasketball.csv', 'r') as f:
     allData = reader
     features = allData[1:]
 
+#These are the arrays for our training and test data that we will fill with our data from the csv
 training_labels = []
 test_labels = []
 test_features = []
@@ -16,16 +26,20 @@ training_features = []
 
 counter = 0
 for item in features:
-    if counter < 30:
+    #Player 1 Training Data
+    if counter < rows[0]:
         training_labels.append(allData[counter][0])
         training_features.append(item)
-    if counter >= 30 and counter < 74:
+    #Player 1 Test Data
+    if counter >= rows[0] and counter < rows[1]:
         test_labels.append(allData[counter][0])
         test_features.append(item)
-    if counter >= 74 and counter < 103:
+    #Player 2 Training Data
+    if counter >= rows[1] and counter < rows[2]:
         training_labels.append(allData[counter][0])
         training_features.append(item)
-    if counter >= 103:
+    #Player 2 Test Data
+    if counter >= rows[2] and counter < rows[3]:
         test_labels.append(allData[counter][0])
         test_features.append(item)
     counter = counter + 1
@@ -33,21 +47,32 @@ for item in features:
 clf = tree.DecisionTreeClassifier()
 clf = clf.fit(training_features, training_labels)
 
+#These are some counter variables that will be used during testing
 counter = 0
 rights = 0.
 total = 0.
+f = open('learningoutput.csv', 'wb')
+
 for item in test_features:
+    result = "Incorrect"
     actual = test_labels[counter]
     
     #clf.predict needs to have an array containing in a single arry, so we need to put item (an array) into another array (itemContainer)
     itemContainer = []
     itemContainer.append(item)
 
+    #Program makes prediciton and checks to see if it's correct
     prediction = clf.predict(itemContainer)
     if actual == prediction:
         rights = rights + 1
+        result = "Correct"
     counter = counter + 1
     total = total + 1
 
+    #Write individual results to csv
+    writer = csv.writer(f, lineterminator="\n")
+    writer.writerow(["Game " + str(counter),"Predicted Player: " + str(prediction[0]),"Actual Player: " + str(actual),result])
+
+f.close()
 perc = (rights/total)*100
 print("Percentage Correct: " + str(perc))

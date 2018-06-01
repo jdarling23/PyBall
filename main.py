eae723b725a0ffb02f01f53d56a0dab4cd5e72f7
James Darling <jdarling@credera.com>
2018-05-31 23:34:31 -0500

    Fixed array issue, program now comapres just Lebron and Westbrook

--- main.py
+++ main.py
@@ -34,11 +34,16 @@ clf = tree.DecisionTreeClassifier()
 clf = clf.fit(training_features, training_labels)
 
 counter = 0
-rights = 0
-total = 0
+rights = 0.
+total = 0.
 for item in test_features:
     actual = test_labels[counter]
-    prediction = clf.predict(item)
+    
+    #clf.predict needs to have an array containing in a single arry, so we need to put item (an array) into another array (itemContainer)
+    itemContainer = []
+    itemContainer.append(item)
+
+    prediction = clf.predict(itemContainer)
     if actual == prediction:
         rights = rights + 1
     counter = counter + 1
